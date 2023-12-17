7e962ca5bc046a9481d31880c6a02760a58efb62
Hafiz-Faizan <fazi4066745@gmail.com>
2023-12-17 23:35:05 +0500

    done

---
+++ src/component/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Homepage = () => {
+  return (
+    <div>
+        <h1>Math Magician</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sed iure ipsum ea a earum cum neque. Atque necessitatibus fuga ut beatae dolorem. Aliquid, iste animi. Dignissimos nisi amet quaerat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore obcaecati iure minus consequatur provident iste repudiandae unde praesentium dolor voluptate? Obcaecati architecto quia eligendi doloremque. Error eveniet eum tempora facilis?</p>
+    </div>
+  )
+}
+export default Homepage;
---
+++ src/component/Qoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Qoute = () => {
+  return (
+    <div>
+        
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore obcaecati iure minus consequatur provident iste repudiandae unde praesentium dolor voluptate? Obcaecati architecto quia eligendi doloremque. Error eveniet eum tempora facilis?</p>
+    </div>
+  )
+}
+export default Qoute;
